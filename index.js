9447359b8e434f2a62e9c9c695d45c23d5d9909f
fabianossilva24 <fabiano.jaragua24@hotmail.com>
2018-10-02 11:58:06 -0300

    adição da agenda

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use('/static', express.static('static'));
+app.use(bodyParser.urlencoded());
+
+app.get('', (req, res) => {
+    res.render('index');
+});
+
+app.get('/views/Nossotrabalho.ejs', (req, res) => {
+    res.render('NossoTrabalho');
+});
+app.get('/views/Parceiros.ejs', (req, res) => {
+    res.render('Parceiros');
+});
+app.get('/views/agenda.ejs', (req, res) => {
+    res.render('agenda');
+});
+app.get('/views/index.ejs', (req, res) => {
+    res.render('index');
+});
+
+
+app.post('', (req, res) => {
+    req.db.collection('parceiro').insert(req.body, (erro) => {
+        res.render('/parceiros');
+    });
+});
+
+app.post('', (req, res) => {
+    let string = `Nome: ${req.body.nome} \nEmail: ${req.body.email} \nTelefone: ${req.body.telefone} \n`;
+})  
+
+
+
+
+
+// app.get('/admin/mensagens', (req, res) => {
+//     req.db.collection('mensagens').find().toArray((erro, dados) => {
+//         res.render('admin-parceiros', {'mensagens': dados});
+//     });
+// });
+
+
+
+
+// app.post('', (req, res) => {
+//     req.db.collection('parceiros').insert(req.body, (erro) => {
+//         console.log(erro);
+//         res.render('obrigado.ejs');
+//     });
+// });
+
+// app.get('/admin/parceiros', (req, res) => {
+//     req.db.collection('parceiros').find().toArray((erro, dados) => {
+//         res.render('admin-parceiros', {'parceiros': dados});
+//     });
+// });
+
+
+app.listen(4000, () => {
+    console.log('Servidor inicializado')
+});
